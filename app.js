cb6a6d2b0138cc22db67add00fda62fa2c7af3d9
dante851 <vignesh.s98@cognizant.com>
2024-11-27 18:23:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var sdk = require("./lib/sdk");
 var config = require("./config");
 var app = new Application(null, config);
 var server = new Server(config, app);
-const express = require('express');
-const appVerbiage = express();
-const customBotActionRoutes = require("./routes/ESICustomBotActionRoutes/ESICustomBotActionRoutes")
-appVerbiage.use("/api/custom-bot-action", customBotActionRoutes);
+const ESICustomBotActionService = require("./service/ESICustomBotActionService");
+verbiage_En_RespData = await ESICustomBotActionService.getVerbiageResponse("ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx");
+verbiage_Fr_RespData = await ESICustomBotActionService.getVerbiageResponse("ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");
 sdk.checkNodeVersion();
 server.start();
 
