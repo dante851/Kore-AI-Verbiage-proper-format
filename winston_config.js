0403f917910b5b3bff6f9a48fb0887369d578217
dante851 <vignesh.s98@cognizant.com>
2024-11-27 20:37:00 +0530

    Update winston_config.js

--- winston_config.js
+++ winston_config.js
@@ -1,18 +1,16 @@
 const winston = require("winston");
-// const { combine, timestamp, json } = winston.format;
+const { combine, timestamp, json } = winston.format;
 const path = require("path");
 const datetime = new Date();
 exports.logger = winston.createLogger({
   level: "info",
   transports: [
     new winston.transports.Console({
-      format: winston.simple(),
+      format: winston.format.simple(),
     }),
     new winston.transports.File({
-      format: winston.combine(winston.timestamp(), winston.json()),
+      format: combine(timestamp(), json()),
       filename: path.resolve(__dirname,`./logs/${datetime.toISOString().slice(0,10)}.log`),
     }),
   ],
 });
-    
-  
