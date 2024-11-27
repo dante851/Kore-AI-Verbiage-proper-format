34dd81f2c60890a59b8fbc21d59c73b2de34e9f9
dante851 <vignesh.s98@cognizant.com>
2024-11-27 19:17:55 +0530

    Update ESIEnterpriseService.js

--- service/ESIEnterpriseService.js
+++ service/ESIEnterpriseService.js
@@ -7,21 +7,13 @@ module.exports = {
    * @returns the id details.
    */
   async getOrderDetails(orderId) {
-    try {
       const response = await axios.get(
         `https://667009f30900b5f8724927c3.mockapi.io/flights/Orderdata?orderId=${orderId}`);
       return response.data;
-    } catch (error) {
-      return error;
-    }
   },
   async getMemberDetails(memberId) {
-    try {
         const response = await axios.get(
           `https://667009f30900b5f8724927c3.mockapi.io/flights/Orderdata?memberId=${memberId}`);
         return response.data;
-      } catch (error) {
-        return error;
-      }
   }
 };
