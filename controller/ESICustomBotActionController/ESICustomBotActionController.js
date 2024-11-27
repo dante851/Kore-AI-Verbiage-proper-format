d6082293fc815154c2700241acfcd87674b879b4
dante851 <vignesh.s98@cognizant.com>
2024-11-27 17:44:27 +0530

    Update ESICustomBotActionController.js

--- controller/ESICustomBotActionController/ESICustomBotActionController.js
+++ controller/ESICustomBotActionController/ESICustomBotActionController.js
@@ -11,13 +11,13 @@ const MODULE_NAME = "ESICustomBotActionController";
 const fs = require('fs');
 const constants = require("../../constants/index");
 const ESICustomBotActionService = require("../../service/ESICustomBotActionService");
-const ENfilePath = '../../ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx';
-const FRfilePath = '../../ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx';
+// const ENfilePath = '../../ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx';
+// const FRfilePath = '../../ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx';
 const path = require('path');
 
 // Get the directory path
-const EN_FILE = fs.readFileSync(path.dirname(ENfilePath));
-const FR_FILE = fs.readFileSync(path.dirname(FRfilePath));
+// const EN_FILE = fs.readFileSync(path.dirname(ENfilePath));
+// const FR_FILE = fs.readFileSync(path.dirname(FRfilePath));
 module.exports = {
   /**
    * Get the Verbiage details from the database.
@@ -31,8 +31,8 @@ module.exports = {
     try {
       const language = req.body.currentLang;
       response = Object.create(constants.serverResponses.success);
-    verbiage_En_RespData = await ESICustomBotActionService.getVerbiageResponse(EN_FILE);
-    verbiage_Fr_RespData = await ESICustomBotActionService.getVerbiageResponse(FR_FILE);
+    verbiage_En_RespData = await ESICustomBotActionService.getVerbiageResponse("ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx");
+    verbiage_Fr_RespData = await ESICustomBotActionService.getVerbiageResponse("ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");
       const verbiageBuilderData =
         language === "fr" ? verbiage_Fr_RespData : verbiage_En_RespData;
     //   let result = verbiageBuilderData.filter(
