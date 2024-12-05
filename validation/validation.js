97c75e214187b8f053db213e395cf1553b1333a6
Vignesh S <vignesh.s98@cognizant.com>
2024-12-05 17:04:41 +0530

    console added

--- validation/validation.js
+++ validation/validation.js
@@ -3,6 +3,8 @@ const constants = require("../constants");
 module.exports = {
   async validateAPIkey(req, res,next) {
     const apiKey = req.headers["ESI_WRAPPER_SERVICE_API_KEY"] || "";
+    console.log("process.env.ESI_WRAPPER_SERVICE_API_KEY",process.env.ESI_WRAPPER_SERVICE_API_KEY)
+    console.log("apikey",apiKey)
     if (apiKey === process.env.ESI_WRAPPER_SERVICE_API_KEY) {
       return next();
     } else {
