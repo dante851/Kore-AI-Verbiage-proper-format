1821e9d5b6f5cccad9cf0a2e8f7457de1aafc83b
Vignesh S <vignesh.s98@cognizant.com>
2024-12-09 15:36:05 +0530

    added map ()

--- utility.js
+++ utility.js
@@ -11,11 +11,22 @@ module.exports = {
     let entityStatus = messageDataWithBotUserSession.entity_status;
     let failedEntity = messageDataWithBotUserSession.failedEntity;
     let orderIdInput = "";
-    let result = verbiage_builder_resp.filter(
-      (ele) => ele.RESPONSE_ID.trim() === responseId
-    );
+    // let result = verbiage_builder_resp.map(
+    //   (ele) => ele.RESPONSE_ID.trim() === responseId
+    // );
+    let result = verbiage_builder_resp.map((ele) => {
+      if (ele.RESPONSE_ID.trim() === responseId) {
+        return {
+          RESPONSE_ID: ele.RESPONSE_ID,
+          WEB_RESPONSE_MSG: ele.WEB_RESPONSE_MSG,
+          ANDROID_RESPONSE_MSG: ele.ANDROID_RESPONSE_MSG,
+          IOS_RESPONSE_MSG: ele.IOS_RESPONSE_MSG,
+          MEDIA_TYPE: ele.MEDIA_TYPE,
+        };
+      }
+    });
 
-    console.log(result)
+    console.log("result",result);
     // Custom Bot Responses Condition
     if (responseId.startsWith("ESI_PHA_ORD_INFO")) {
       switch (responseId) {
